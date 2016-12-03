8d1c2cfd3989be9e54b4695dce3541ab22e09a46
wust LiTao <705398096@qq.com>
2016-12-03 15:06:12 +0800

    Initial Commit

---
+++ UPTableViewDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UPTableViewDisplay
+//
+//  Created by wust_LiTao on 2016/12/3.
+//  Copyright © 2016年 李锦民. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UPTableViewDisplay/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UPTableViewDisplay
+//
+//  Created by wust_LiTao on 2016/12/3.
+//  Copyright © 2016年 李锦民. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
